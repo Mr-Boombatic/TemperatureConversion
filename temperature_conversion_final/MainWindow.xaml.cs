561785480f725fbe14f1c695537085e87c43d35e
AlexandrTel <54598466+AlexandrTel@users.noreply.github.com>
2021-08-04 18:05:16 +0300

    new file:   .gitignore 	new file:   .vs/temperature_conversion_final/DesignTimeBuild/.dtbcache 	new file:   .vs/temperature_conversion_final/DesignTimeBuild/.dtbcache.v2 	new file:   .vs/temperature_conversion_final/v16/Server/sqlite3/db.lock 	new file:   .vs/temperature_conversion_final/v16/Server/sqlite3/storage.ide 	new file:   temperature_conversion_final.sln 	new file:   temperature_conversion_final/App.xaml 	new file:   temperature_conversion_final/App.xaml.cs 	new file:   temperature_conversion_final/AssemblyInfo.cs 	new file:   temperature_conversion_final/MainWindow.xaml 	new file:   temperature_conversion_final/MainWindow.xaml.cs 	new file:   temperature_conversion_final/temperature_conversion_final.csproj

---
+++ temperature_conversion_final/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace temperature_conversion_final
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private bool counting;
+        private void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var source = (TextBox)sender;
+
+            if (counting != true)
+            {
+                counting = true;
+                Celsius.BorderBrush = Brushes.Black;
+                Kelvin.BorderBrush = Brushes.Black;
+                Fahrenheit.BorderBrush = Brushes.Black;
+
+                if (source == Fahrenheit)
+                {
+                    try
+                    {
+                        Celsius.Text = (0.5555555 * (float.Parse(Fahrenheit.Text) - 32)).ToString();
+                        Kelvin.Text = (float.Parse(Celsius.Text) + 273.15).ToString();
+                    }
+                    catch
+                    {
+                        Celsius.Text = "";
+                        Kelvin.Text = "";
+                        Celsius.BorderBrush = Brushes.Red;
+                        Kelvin.BorderBrush = Brushes.Red;
+                        counting = false;
+                    }
+                }
+                else if (source == Celsius)
+                {
+                    try
+                    {
+                        Fahrenheit.Text = (1.8 * (float.Parse(Celsius.Text) + 32)).ToString();
+                        Kelvin.Text = (float.Parse(Celsius.Text) + 273.15).ToString();
+                    }
+                    catch
+                    {
+                        Kelvin.Text = ""; 
+                        Fahrenheit.Text = "";
+                        Kelvin.BorderBrush = Brushes.Red;
+                        Fahrenheit.BorderBrush = Brushes.Red;
+                        counting = false;
+                    }
+                }
+                else if (source == Kelvin)
+                {
+                    try
+                    {
+                        Celsius.Text = (float.Parse(Kelvin.Text) - 273.15).ToString();
+                        Fahrenheit.Text = (1.8 * (float.Parse(Celsius.Text) + 32)).ToString();
+                    }
+                    catch
+                    {
+                        Celsius.Text = "";
+                        Fahrenheit.Text = "";
+                        Celsius.BorderBrush = Brushes.Red;
+                        Fahrenheit.BorderBrush = Brushes.Red;
+                        counting = false;
+                    }
+                }
+
+                counting = false;
+            }
+        }
+    }
+}
